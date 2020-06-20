edba5e66649a0572f23c66bad4bf06314270016d
olszowskijacek <jacek.olszowski1@gmail.com>
2020-06-20 16:12:31 +0200

    quotation add bar logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'best-quotes-app';
+  showForm = false;
+
+  onSwitchForm() {
+    this.showForm = !this.showForm;
+  }
 }
